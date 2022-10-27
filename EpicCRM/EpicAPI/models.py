8a14b02de85542b9044d7f037577f163ce84c641
notRoot <kalimba@pc-34.home>
2022-10-27 08:47:52 +0200

    Added Contract, Event and Client to models

--- EpicCRM/EpicAPI/models.py
+++ EpicCRM/EpicAPI/models.py
@@ -19,42 +19,45 @@ class EpicTeamMember(AbstractUser):
                             verbose_name="Epic Events Team's Choice")
 
 
-# class Client(models.Model):
+class Client(models.Model):
 
-#     first_name = models.CharField(max_length=25)
-#     last_name = models.CharField(max_length=25)
-#     email = models.EmailField()
-#     phone = models.CharField(20)
-#     mobile = models.CharField(20)
-#     company_name = models.CharField(250)
-#     date_created = models.DateTimeField(auto_now_add=True)
-#     date_updated = models.DateTimeField(auto_now=True)
-#     sales_contact = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                                         on_delete=models.SET_NULL)
-#     existing_client = models.BooleanField(help_text="True if client with active or past\
-#                                           events, False is not")
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20)
+    company_name = models.CharField(max_length=250)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    sales_contact = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                        on_delete=models.SET_NULL, null=True,
+                                        blank=True)
+    existing_client = models.BooleanField(help_text="True if client with active or past\
+                                          events, False is not")
 
-# class Contract(models.Model):
+class Contract(models.Model):
 
-#     sales_contact = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                                         on_delete=models.SET_NULL)
-#     client = models.ForeignKey(Client, null=False, on_delete=models.SET_NULL)
-#     date_created = models.DateTimeField(auto_now_add=True)
-#     date_updated = models.DateTimeField(auto_now=True)
-#     status = models.BooleanField(help_text="True if paid, False if payment pending")
-#     amount = models.FloatField()
-#     payment_due = models.DateTimeField()
-#     # need to add foreign key towards Event ?
+    sales_contact = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                        on_delete=models.SET_NULL, null=True,
+                                        blank=True)
+    client = models.ForeignKey(Client, null=False, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(help_text="True if paid, False if payment pending")
+    amount = models.FloatField()
+    payment_due = models.DateTimeField()
+    # need to add foreign key towards Event ?
 
-# class Event(models.Model):
+class Event(models.Model):
 
-#     client = models.ForeignKey(Client, null=False, on_delete=models.SET_NULL)
-#     contract = models.ForeignKey(Contract, null=False, on_delete=models.SET_NULL)
-#     date_created = models.DateTimeField(auto_now_add=True)
-#     date_updated = models.DateTimeField(auto_now=True)
-#     support_contact = models.ForeignKey(settings.AUTH_USER_MODEL,
-#                                         on_delete=models.SET_NULL)
-#     event_status = models.BooleanField(help_text="True if ongoing, False if over")
-#     attendees = models.IntegerField()
-#     event_date = models.DateTimeField()
-#     notes = models.TextField(max_length=1000)
+    client = models.ForeignKey(Client, null=False, on_delete=models.CASCADE)
+    contract = models.ForeignKey(Contract, null=False, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    support_contact = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                        on_delete=models.SET_NULL, null=True,
+                                        blank=True)
+    event_status = models.BooleanField(help_text="True if ongoing, False if over")
+    attendees = models.IntegerField()
+    event_date = models.DateTimeField()
+    notes = models.TextField(max_length=1000)
